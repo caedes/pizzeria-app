082b8c3960951c9657c216c88c31385cacd7f989
Romain Laurent <romain.laurent@cdiscount.com>
2020-06-03 17:34:54 +0200

    feat: 🎸 add Header

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { Button, CssBaseline } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 
 import { theme } from "./theme";
+import Header from "../Header";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <header>
-        <h1>Pizzeria Nala</h1>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </header>
+      <Header shoppingCartCount={3} />
     </ThemeProvider>
   );
 }
---
+++ src/Header/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  AppBar,
+  Badge,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { number } from "prop-types";
+
+const useStyles = makeStyles({
+  title: {
+    flexGrow: 1,
+  },
+});
+
+export default function Header({ shoppingCartCount }) {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          Pizzeria Nala
+        </Typography>
+        <IconButton aria-label={`${shoppingCartCount} pizzas`} color="inherit">
+          <Badge badgeContent={shoppingCartCount} color="secondary">
+            <ShoppingCartIcon />
+          </Badge>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+Header.defaultProps = {
+  shoppingCartCount: 0,
+};
+
+Header.propTypes = {
+  shoppingCartCount: number,
+};
