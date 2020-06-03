0299c73e4f2acbb013f0902500cf2a175f309990
Romain Laurent <romain.laurent@cdiscount.com>
2020-06-03 15:57:02 +0200

    feat: 🎸 add MUI theme

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Button, CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+import { theme } from "./theme";
 
 export default function App() {
   return (
-    <header>
-      <h1>Pizzeria Nala</h1>
-    </header>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <header>
+        <h1>Pizzeria Nala</h1>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </header>
+    </ThemeProvider>
   );
 }
